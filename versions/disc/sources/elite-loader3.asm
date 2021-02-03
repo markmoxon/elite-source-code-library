88a162ab28d2360c303829570849819b61ded79c
Mark Moxon <mark@moxon.net>
2021-02-03 10:48:13 +0000

    Small changes

--- versions/disc/sources/elite-loader3.asm
+++ versions/disc/sources/elite-loader3.asm
@@ -438,26 +438,51 @@ ORG &0B00
  LDA #HI(S%+6)
  STA WRCHV+1
 
- SEC                    \ Run a checksum on the docked game code
- LDY #0
- STY &70
+ SEC                    \ Set the C flag so the checksum we calculate in A
+                        \ starts with an initial value of 18 (17 plus carry)
 
- LDX #&11
- TXA
+ LDY #&00               \ Set Y = 0 to act as a byte pointer
+
+ STY ZP                 \ Set the low byte of ZP(1 0) to 0, so ZP(1 0) always
+                        \ points to the start of a page
+
+ LDX #&11               \ Set X = &11, so ZP(1 0) will point to &1100 when we
+                        \ stick X in ZP+1 below
+
+ TXA                    \ Set A = &11 = 17, to set the intial value of the
+                        \ checksum to 18 (17 plus carry)
 
 .l1
 
- STX &71
- ADC (&70),Y
- DEY
- BNE l1
+ STX ZP+1               \ Set the high byte of ZP(1 0) to the page number in X
 
- INX
- CPX #&54
- BCC l1
- CMP &55FF
+ ADC (ZP),Y             \ Set A = A + the Y-th byte of ZP(1 0)
 
- BNE P%
+ DEY                    \ Decrement the byte pointer
+
+ BNE l1                 \ Loop back to add the next byte until we have added the
+                        \ whole page
+
+ INX                    \ Increment the page number in X
+
+ CPX #&54               \ Loop back to checksum the next page until we have
+ BCC l1                 \ checked up to (but not including) page &54
+
+ CMP &55FF              \ Compare the checksum with the value in &55FF, which is
+                        \ in the docked file we just loaded, in the byte before
+                        \ the ship hanger blueprints at XX21
+
+IF _REMOVE_CHECKSUMS
+
+ NOP                    \ If we have disabled checksums, then ignore the result
+ NOP
+
+ELSE
+
+ BNE P%                 \ If the checksums don't match then enter an infinite
+                        \ loop, which hangs the computer
+
+ENDIF
 
  JMP S%+3               \ Jump to the second entry in the main docked code's S%
                         \ workspace to start a new game

104d3420b27a27589ce635447e9174c347856160
Mark Moxon <mark@moxon.net>
2021-04-03 16:15:30 +0100

    Minor fix to commentary

--- library/common/main/subroutine/rdkey.asm
+++ library/common/main/subroutine/rdkey.asm
@@ -33,7 +33,7 @@ ENDIF
 \
 \ Returns:
 \
-IF _CASSETTE_VERSION OR _DISC_DOCKED OR _6502SP_VERSION \ Comment
+IF _CASSETTE_VERSION OR _DISC_VERSION OR _6502SP_VERSION \ Comment
 \   X                   If a key is being pressed, X contains the internal key
 \                       number, otherwise it contains 0
 ELIF _MASTER_VERSION

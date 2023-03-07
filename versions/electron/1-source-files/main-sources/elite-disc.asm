fca6880e542175493a57bbfe6e5b05edb28649c7
Mark Moxon <mark@moxon.net>
2023-03-07 12:18:21 +0000

    Remove cassette progress window from Electron disc version's Acornsoft loading screen

--- versions/electron/1-source-files/main-sources/elite-disc.asm
+++ versions/electron/1-source-files/main-sources/elite-disc.asm
@@ -28,7 +28,7 @@
 \ ******************************************************************************
 
 PUTFILE "versions/electron/1-source-files/boot-files/$.!BOOT.bin", "!BOOT", &FFFFFF, &FFFFFF
-PUTFILE "versions/electron/1-source-files/basic-programs/$.ELITE.bin", "ELITE", &FF0E00, &FF8023
+PUTFILE "versions/electron/1-source-files/basic-programs/$.ELITE-disc.bin", "ELITE", &FF0E00, &FF8023
 PUTFILE "versions/electron/3-assembled-output/ELITECO.bin", "ELITECO", &000000, &FFFFFF
 PUTFILE "versions/electron/3-assembled-output/ELITEDA.bin", "ELITEDA", &FF4400, &FF5200
 PUTFILE "versions/electron/3-assembled-output/README.txt", "README", &FFFFFF, &FFFFFF

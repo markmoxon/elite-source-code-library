1abbd891f3476647dc98452d84484cb9343886af
Mark Moxon <mark@moxon.net>
2023-08-30 10:30:09 +0100

    Finished documenting the ship hangar routines

--- versions/nes/1-source-files/main-sources/elite-source-bank-7.asm
+++ versions/nes/1-source-files/main-sources/elite-source-bank-7.asm
@@ -6862,7 +6862,8 @@ INCLUDE "library/nes/main/subroutine/loin_part_7_of_7.asm"
  STY T                  \ Set A = R + Y
  LDA R                  \       = pixels to left draw + current pixel row
  ADC T                  \
-                        \ So A contains ???
+                        \ So A contains the total number of pixels left to draw
+                        \ in our line
 
  SBC #7                 \ At this point the C flag is clear, as the above
                         \ addition won't overflow, so this sets A = R + Y - 8

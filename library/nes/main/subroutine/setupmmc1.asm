dfbad9a5dfd190faa09defb4a827d1b578fbaa99
Mark Moxon <mark@moxon.net>
2023-11-01 17:01:22 +0000

    Updated MMC1 register commentary

--- library/nes/main/subroutine/setupmmc1.asm
+++ library/nes/main/subroutine/setupmmc1.asm
@@ -16,16 +16,16 @@
  LSR A                  \     overrides the horizontal mirroring set in the iNES
  STA &9FFF              \     header)
  LSR A                  \
- STA &9FFF              \   * Bits 2,3 set = PRG ROM bank mode 3 = fix ROM bank
+ STA &9FFF              \   * Bits 2,3 set = PRG-ROM bank mode 3 = fix ROM bank
  LSR A                  \     7 at &C000 and switch 16K ROM banks at &8000
  STA &9FFF              \
-                        \   * Bit 4 clear = CHR ROM bank mode 0 = switch 8K at
+                        \   * Bit 4 clear = CHR-ROM bank mode 0 = switch 8K at
                         \     a time
 
  LDA #0                 \ Set the MMC1 CHR bank 0 register (which is mapped to
- STA &BFFF              \ &A000-&BFFF) to map CHR-ROM to &0000, though this has
- LSR A                  \ no effect as we have no CHR-ROM (we have CHR-RAM
- STA &BFFF              \ instead, which is always mapped to &6000-&7FFF)
+ STA &BFFF              \ &A000-&BFFF) to map the first 4K of CHR-RAM to &0000
+ LSR A                  \ in the PPU, so pattern table 0 is writable RAM
+ STA &BFFF
  LSR A
  STA &BFFF
  LSR A
@@ -34,9 +34,9 @@
  STA &BFFF
 
  LDA #0                 \ Set the MMC1 CHR bank 1 register (which is mapped to
- STA &DFFF              \ &C000-&DFFF) to map CHR-ROM to &1000, though this has
- LSR A                  \ no effect as we have no CHR-ROM (we have CHR-RAM
- STA &DFFF              \ instead, which is always mapped to &6000-&7FFF)
+ STA &DFFF              \ &C000-&DFFF) to map the second 4K of CHR-RAM to &1000
+ LSR A                  \ in the PPU, so pattern table 1 is writable RAM
+ STA &DFFF
  LSR A
  STA &DFFF
  LSR A

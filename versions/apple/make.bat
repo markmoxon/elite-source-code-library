9d90a7fbf47b9d1a53e02aba7c367f2d81fd9f4b
Mark Moxon <mark@moxon.net>
2024-10-23 13:33:32 +0100

    Change disc to disk throughout Apple and Commodore builds

--- versions/apple/make.bat
+++ versions/apple/make.bat
@@ -2,4 +2,5 @@
 SETLOCAL
 SET BEEBASM=C:\Users\user\bin\beebasm.exe
 SET PYTHON=C:\Users\user\AppData\Local\Microsoft\WindowsApps\python.exe
+SET DISKM8=C:\Users\user\bin\diskm8.exe
 2-build-files\make %*

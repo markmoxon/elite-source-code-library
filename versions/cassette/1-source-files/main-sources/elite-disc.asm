5682c0a7f871c2934ac4188a5c55b4c01f54878a
Mark Moxon <mark@moxon.net>
2024-11-15 13:27:52 +0000

    Rename BASIC loader programs for cassette version to be consistent with other versions

--- versions/cassette/1-source-files/main-sources/elite-disc.asm
+++ versions/cassette/1-source-files/main-sources/elite-disc.asm
@@ -46,7 +46,7 @@ IF _DISC
 ELSE
 
  PUTFILE "versions/cassette/1-source-files/boot-files/$.!BOOT.bin", "!BOOT", &FFFFFF, &FFFFFF
- PUTFILE "versions/cassette/1-source-files/basic-programs/$.ELITEc.bin", "ELITE", &FF1900, &FF8023
+ PUTFILE "versions/cassette/1-source-files/basic-programs/$.ELITE-cassette.bin", "ELITE", &FF1900, &FF8023
 \PUTFILE "versions/cassette/3-assembled-output/ELITE.bin", "ELITEdata", &FF0E00, &FF1D00
 \PUTFILE "versions/cassette/3-assembled-output/ELTcode.bin", "ELITEcode", &000E00, &000132
  PUTFILE "versions/cassette/3-assembled-output/ELITE.bin", "ELITEda", &FF0E00, &FF1D00

dfedf783caee47ee4c7a03d72e10aed11de294a6
Mark Moxon <mark@moxon.net>
2025-01-16 16:11:49 +0000

    Documented the CPIX pixel-plotting routine

--- versions/apple/1-source-files/main-sources/elite-source.asm
+++ versions/apple/1-source-files/main-sources/elite-source.asm
@@ -4834,7 +4834,24 @@ INCLUDE "library/common/main/subroutine/loin_part_7_of_7-loinq_part_7_of_7.asm"
 \       Name: CPIX
 \       Type: Subroutine
 \   Category: Drawing pixels
-\    Summary: Draw a colour pixel ???
+\    Summary: Draw a colour pixel
+\
+\ ------------------------------------------------------------------------------
+\
+\ Arguments:
+\
+\   X                   The screen x-coordinate of the pixel
+\
+\   A                   The screen y-coordinate of the pixel
+\
+\   COL                 The pixel colour, as an offset into the MASKT table
+\
+\ ------------------------------------------------------------------------------
+\
+\ Returns:
+\
+\   Y                   The byte offset within the pixel row of the byte that
+\                       contains the pixel
 \
 \ ******************************************************************************
 
@@ -4877,32 +4894,83 @@ INCLUDE "library/common/main/subroutine/loin_part_7_of_7-loinq_part_7_of_7.asm"
                         \ pixel we want to draw (as X contains the x-coordinate
                         \ of the pixel)
 
- LDA #0
- CPY #6
- BNE P%+4
- LDA #&81
- STA T3
- LDA TWOS2,Y
- STA R
- LDA SCTBX2,X
- AND #1
- ORA COL
- TAY
- LDA MASKT+1,Y
- AND T3
- STA T3
- LDA MASKT,Y
- AND R
- STA R
- LDY SCTBX2,X
- EOR (SC),Y
- STA (SC),Y
- LDA T3
- BEQ CPR1
- INY
- EOR (SC),Y
- STA (SC),Y
- DEY
+ LDA #0                 \ Set A = 0 to use as the pixel mask for the next pixel
+                        \ byte along, so by default we don't change anything in
+                        \ the next pixel byte
+
+ CPY #6                 \ If Y = 6 then then the bit number for the pixel is bit
+ BNE P%+4               \ 6 and we will need to spill into the next pixel byte,
+ LDA #%10000001         \ so set A = %10000001 to use as the pixel mask for the
+                        \ next pixel byte along
+
+ STA T3                 \ Store the pixel mask for the next pixel byte in T3
+
+ LDA TWOS2,Y            \ Fetch a two-bit pixel byte with the pixels set at
+ STA R                  \ position Y and store it in R so we can use it as a
+                        \ mask for the bits we want to change in the pixel byte
+
+ LDA SCTBX2,X           \ Using the lookup table at SCTBX2, set A to the byte
+                        \ number within the pixel row that contains the pixel we
+                        \ want to draw (as X contains the x-coordinate of the
+                        \ start of the line)
+
+ AND #1                 \ Set Y to the colour in COL, plus 1 if the byte number
+ ORA COL                \ within the pixel row is odd
+ TAY                    \
+                        \ We can use this to fetch the correct pixel bytes from
+                        \ MASKT that we can poke into screen memory to draw a
+                        \ continuous line of the relevant colour
+                        \
+                        \ Bytes #0 and #1 of the relevant entry in MASKT contain
+                        \ the bit pattern for when the first byte is placed in
+                        \ an even-numbered pixel byte (counting along the pixel
+                        \ row), while bytes #1 and #2 contain the bit pattern
+                        \ for when the first byte is placed in an odd-numbered
+                        \ pixel byte
+                        \
+                        \ So Y now points to the correct MASKT entry for the
+                        \ start of the line, because it points to byte #0 in
+                        \ offset COL if the byte number within the pixel row is
+                        \ even, or byte #1 in offset COL if the byte number
+                        \ within the pixel row is odd
+
+ LDA MASKT+1,Y          \ Set T3 to the correct pixel byte for drawing our pixel
+ AND T3                 \ in the next pixel byte along, by combining the colour
+ STA T3                 \ mask from MASKT+1 with the pixel mask in T3
+
+ LDA MASKT,Y            \ Set R to the correct pixel byte for drawing our pixel
+ AND R                  \ in the first pixel byte, by combining the colour mask
+ STA R                  \ from MASKT with the pixel mask in A (the byte is also
+                        \ stored in R, but this isn't used anywhere)
+
+                        \ So A contains the pattern for the byte at the pixel
+                        \ coordinates, and T3 contains the pattern for the next
+                        \ byte along
+
+ LDY SCTBX2,X           \ Using the lookup table at SCTBX2, set Y to the byte
+                        \ number within the pixel row that contains the pixel
+                        \ at X
+
+ EOR (SC),Y             \ Draw the pixel pattern in A into the Y-th pixel byte
+ STA (SC),Y             \ on the correct pixel row, using EOR logic to merge the
+                        \ pattern with whatever is already on-screen
+
+ LDA T3                 \ Set A to the pattern for the next byte along
+
+ BEQ CPR1               \ If T3 is zero then there is no need to write to the
+                        \ next byte along, so jump to CPR1 to return from the
+                        \ subroutine
+
+ INY                    \ Increment Y to move to the next pixel byte to the
+                        \ right
+
+ EOR (SC),Y             \ Draw the pixel pattern in A into the Y-th pixel byte
+ STA (SC),Y             \ on the correct pixel row, using EOR logic to merge the
+                        \ pattern with whatever is already on-screen
+
+ DEY                    \ Decrement Y to move back to the previous pixel byte,
+                        \ so we can return this value (though this doesn't seem
+                        \ to be used anywhere)
 
 .CPR1
 

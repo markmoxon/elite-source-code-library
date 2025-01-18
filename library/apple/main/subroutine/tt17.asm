510265bb077eab0510557d31bdaef9ca53b0d938
Mark Moxon <mark@moxon.net>
2025-01-18 14:33:04 +0000

    Finish documenting the keyboard and joystick routines

--- library/apple/main/subroutine/tt17.asm
+++ library/apple/main/subroutine/tt17.asm
@@ -77,47 +77,61 @@
 
  TAX                    \ Copy A to X, so X contains the joystick roll value
 
-IF _SOURCE_DISK_CODE_FILES
-
- LDA &80                \ ???
- RTS
- LDA &85
- BIT &86
- BPL L794B
- LDA #&01
-
-.L794B
-
- BIT &87
-
- BPL L7951
- ASL A
- ASL A
-
-.L7951
-
- TAY
-
- LDA #&00
- STA &83
- STA &84
- STA &85
- STA &86
- STA &87
-
-ENDIF
-
  LDA thiskey            \ Fetch the key pressed from thiskey in the key logger
 
  RTS                    \ Return from the subroutine
+
+IF _SOURCE_DISK_CODE_FILES
+
+                        \ This code, which is only present in the CODE* binary
+                        \ files already on the source disk, is never called, but
+                        \ it would allow faster movement of the crosshairs on
+                        \ the system charts if fire was also pressed
+                        \
+                        \ It sets Y as follows:
+                        \
+                        \   * -1 if the ship is being pulled up
+                        \
+                        \   * -4 if the ship is being pulled up while the fire
+                        \        key is being pressed
+                        \
+                        \   * +1 if the ship is being pitched down
+                        \
+                        \   * +4 if the ship is being pitched down while the
+                        \        firekey is being pressed
+                        \
+                        \   * 0 otherwise
+
+ LDA KY5                \ Set A to the state of the "X" key (pull up), which
+                        \ will be &FF (i.e. -1) if the key is being pressed
+
+ BIT KY6                \ If the "S" key (pitch down) is being pressed, set
+ BPL P%+4               \ A = 1
+ LDA #1
+
+ BIT KY7                \ If the "A" key (fire lasers) is being pressed, double
+ BPL P%+4               \ the value in A
+ ASL A
+ ASL A
+
+ TAY                    \ Copy the value of A into Y
+
+ LDA #0                 \ Clear the key logger entries for the primary flight
+ STA KY3                \ controls
+ STA KY4
+ STA KY5
+ STA KY6
+ STA KY7
+
+ENDIF
 
 .TJ1
 
 IF _IB_DISK
 
- LDA #0
- STA &8C
- STA &8E
+ LDA #0                 \ Clear the key logger entries for "M" (fire missile)
+ STA KY16               \ and "J" (in-system jump)
+ STA KY18
 
 ENDIF
 
@@ -126,51 +140,65 @@ ENDIF
  LDX #0                 \ Set the initial values for the results, X = Y = 0,
  LDY #0                 \ which we now increase or decrease appropriately
 
- CMP #8                 \ ???
-
 IF _IB_DISK
 
- BEQ &794B
- CMP #&4A
+ CMP #8                 \ If left arrow (CTRL-H) was pressed, skip the next two
+ BEQ P%+6               \ instructions to set X = X - 1
 
-ENDIF
+ CMP #'J'               \ If "J" was not pressed, skip the next instruction
+ BNE P%+3
 
+ DEX                    \ Either left arrow (CTRL-H) or "J" was pressed, so set
+                        \ X = X - 1
+
+ CMP #21                \ If right arrow (CTRL-U) was pressed, skip the next two
+ BEQ P%+6               \ instructions to set X = X + 1
+
+ CMP #'K'               \ If "K" was not pressed, skip the next instruction
+ BNE P%+3
+
+ INX                    \ Either right arrow (CTRL-U) or "K" was pressed, so set
+                        \ X = X + 1
+
+ CMP #10                \ If down arrow (CTRL-J) was pressed, skip the next two
+ BEQ P%+6               \ instructions to set Y = Y - 1
+
+ CMP #'M'               \ If "M" was not pressed, skip the next instruction
+ BNE P%+3
+
+ DEY                    \ Either down arrow (CTRL-J) or "M" was pressed, so set
+                        \ Y = Y - 1
+
+ CMP #11                \ If up arrow (CTRL-K) was pressed, skip the next two
+ BEQ P%+6               \ instructions to set Y = Y + 1
+
+ CMP #'I'               \ If "I" was not pressed, skip the next instruction
+ BNE P%+3
+
+ INY                    \ Either up arrow (CTRL-K) or "I" was pressed, so set
+                        \ Y = Y + 1
+
+ELIF _SOURCE_DISK
+
+ CMP #8                 \ If left arrow (CTRL-H) was pressed, set X = X - 1
  BNE P%+3
  DEX
- CMP #21
 
-IF _IB_DISK
-
- BEQ P%+6
- CMP #&4B
-
-ENDIF
-
+ CMP #21                \ If right arrow (CTRL-U) was pressed, set X = X + 1
  BNE P%+3
  INX
- CMP #10
 
-IF _IB_DISK
-
- BEQ P%+6
- CMP #&4D
-
-ENDIF
-
+ CMP #10                \ If down arrow (CTRL-J) was pressed, set Y = Y - 1
  BNE P%+3
  DEY
- CMP #11
 
-IF _IB_DISK
-
- BEQ P%+6
- CMP #&49
+ CMP #11                \ If up arrow (CTRL-K) was pressed, set Y = Y + 1
+ BNE P%+3
+ INY
 
 ENDIF
 
- BNE P%+3
- INY
- RTS
+ RTS                    \ Return from the subroutine
 
 .TJS1
 

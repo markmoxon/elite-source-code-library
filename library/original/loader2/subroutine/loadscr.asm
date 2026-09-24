c03f7cced3a8ae0622779b68d0b23c5d26b6715d
Mark Moxon <mark@moxon.net>
2026-09-24 13:32:25 +0100

    Fixed an Electron-related loader comment

--- library/original/loader2/subroutine/loadscr.asm
+++ library/original/loader2/subroutine/loadscr.asm
@@ -147,6 +147,11 @@ IF _DISC_VERSION \ Platform
  BIT S                  \ If bit 7 of S is set (this is an Electron), jump to
  BMI jsr5               \ jsr5
 
+.jsr4
+
+                        \ If we get here then this is a BBC Micro, so we can
+                        \ show the game's name in the mode 7 screen
+
 ELIF _ELECTRON_VERSION
 
  BIT S                  \ If bit 7 of S is set (this is an Electron), jump to
@@ -244,12 +249,11 @@ ELIF _ELECTRON_VERSION
 
  RTS                    \ Return from the PROT1 subroutine
 
-ENDIF
-
 .jsr4
 
-                        \ If we get here then this is a BBC Micro, so we can
-                        \ show the game's name in the mode 7 screen
+                        \ If we get here then this is an Electron
+
+ENDIF
 
  JSR prstr - PROT1      \ Call prstr to print the following characters,
                         \ restarting from the NOP instruction (this destination
